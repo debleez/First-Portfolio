002abcd4b70aff8e69ff3e581a29a84ba59fa973
Blessing Ethel <blessingethel1@gmail.com>
2024-10-01 15:46:07 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function togglemenue() {
+  const menue = document.querySelector(".menue-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menue.classList.toggle("open");
+  icon.classList.toggle("open");
+}
